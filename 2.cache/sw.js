e14cd9800c998cfc77d187ce8ca38282165ef18b
zetian <ul_sniper@foxmail.com>
2020-12-29 19:59:06 +0800

    缓存接口

--- 2.cache/sw.js
+++ 2.cache/sw.js
@@ -23,7 +23,21 @@ self.addEventListener('fetch', (event) => {
           if (response) {
             return response;
           }
-          return fetch(event.request);
+
+          const fetchRequest = event.request.clone()
+
+          return fetch(fetchRequest).then(response => {
+            if(!response || response.status!== 200 || response.type !== 'basic'){
+              return response
+            }
+
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseToCache)
+            })
+
+            return response
+          })
         }
       )
     );
